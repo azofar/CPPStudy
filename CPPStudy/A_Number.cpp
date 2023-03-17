a02a3a95f8e704ac25ccda873607f89ba9740c3a
azofar <gnswhd03@gmail.com>
2023-03-17 11:07:18 +0900

    230317

--- CPPStudy/A_Number.cpp
+++ CPPStudy/A_Number.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <string>
 
 using namespace std;
@@ -43,4 +43,4 @@ int main()
     cout << "n : " << n << endl;
 
     return 0;
-}
+}*/
---
+++ CPPStudy/Simple_Adder_Practice.cpp
@@ -0,0 +1,18 @@
+/*#include <iostream>
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	
+	std::cout << "첫 번째 숫자를 입력하세요. : ";
+	std::cin >> num1;
+
+	std::cout << "두 번째 숫자를 입력하세요. : ";
+	std::cin >> num2;
+
+	int result = num1 + num2; // int num1 과 num2 바로 밑에 int result를 선언 해버리면 0+0=0 값이 미리 들어가버림.
+	std::cout << "두 숫자의 합은 " << result << "입니다." << std::endl;
+
+	return 0;
+}*/
